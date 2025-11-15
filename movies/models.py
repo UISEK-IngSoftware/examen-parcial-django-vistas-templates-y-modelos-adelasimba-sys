66a072fa5119347ed82e5bc3296f751b20ecb639
adelasimba-sys <adelasimba9b@gmail.com>
2025-11-15 02:19:46 -0500

    Proyecto Django completado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    director_name = models.CharField(max_length=150)
+    year_published = models.PositiveIntegerField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} ({self.year_published})"
